dab4f0f5689aa37e68bf3acf6672ad511e22833d
AndresRipe <andresripe333@gmail.com>
2025-11-17 22:00:39 -0500

    primera integracion

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'if25yp',
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Aquí irían tus listeners o plugins si los necesitas
     },
+    viewportWidth: 1500,
+    viewportHeight: 900,
+    chromeWebSecurity: false,
+    defaultCommandTimeout: 15000,
+    pageLoadTimeout: 15000
   },
 });
